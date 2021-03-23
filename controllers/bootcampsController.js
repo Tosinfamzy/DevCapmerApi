546d5d2a926a016fddb1022cf962da805e5911e9
tosin <famurewatosin@gmail.com>
2021-03-23 19:16:00 +0000

    Basic CRUDfunctionalities with mongo

--- controllers/bootcampsController.js
+++ controllers/bootcampsController.js
@@ -1,34 +1,83 @@
+const Bootcamp = require('../models/bootcamp')
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootCamps = (req, res, next) => {
-    res.status(200).json({success: true, msg: 'Show all bootcamps'})
+exports.getBootCamps = async(req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find()
+        res.status(200).json({success: true, count:bootcamps.length ,data: bootcamps})
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            error:error
+        })
+    }
+
 }
 
 // @desc    Get single bootcamps
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootCamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: `Get bootcamp ${req.params.id}`})
+exports.getBootCamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id)
+        if (!bootcamp) {
+            res.status(404).json({success:false, data:`${req.params.id} not found `});
+        }
+        res.status(200).json({success: true, data: bootcamp})
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            error:error
+        })
+    }
 }
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootCamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: 'Create bootcamps'})
+exports.createBootCamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.create(req.body)
+        res.status(201).json({success: true, data: bootcamp})
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            error:error
+        })
+    }
+
 }
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootCamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`})
+exports.updateBootCamp = async(req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+        runValidators: true
+    })
+    if (!bootcamp) {
+        res.status(404).json({success:false, data:`${req.params.id} not found `});
+    }
+    res.status(200).json({success: true, data: bootcamp})
 }
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootCamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: 'Show all bootcamps'})
+exports.deleteBootCamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+        if (!bootcamp) {
+            res.status(404).json({success:false, data:`${req.params.id} not found `});
+        }
+        res.status(200).json({success: true, data: {}})
+    } catch (error) {
+        res.status(400).json({
+            success:false,
+            error:error
+        })
+    }
+
 }
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 dotenv.config({path: './config/config.env'})
 connectDB()
-
+app.use(express.json())
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
