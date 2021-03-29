cf842e19bebfb6f433b1bb3f787dab57f074c58a
tosin <famurewatosin@gmail.com>
2021-03-29 21:51:17 +0100

    mongoose hooks and slug creation

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -100,5 +101,9 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
       },
+})
+
+BootcampSchema.pre('save', function(){
+  this.slug = slugify(this.name,{lower:true})
 })
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
