4f9fb5181996ecb075f6b605ed6af1992af06668
tosin <famurewatosin@gmail.com>
2021-04-01 23:20:18 +0100

    Add sorting and selecting advanced queries

--- controllers/bootcampsController.js
+++ controllers/bootcampsController.js
@@ -1,103 +1,123 @@
-const ErrorResponse = require('../utils/errorResponse')
-const Bootcamp = require('../models/bootcamp')
-const geocoder = require('../utils/geocoder')
+const ErrorResponse = require("../utils/errorResponse");
+const Bootcamp = require("../models/bootcamp");
+const geocoder = require("../utils/geocoder");
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootCamps = async(req, res, next) => {
-    try {
-        const bootcamps = await Bootcamp.find()
-        res.status(200).json({success: true, count:bootcamps.length ,data: bootcamps})
-    } catch (error) {
-        next(new ErrorResponse(`Bootcampp not found`, 404))
+exports.getBootCamps = async (req, res, next) => {
+  try {
+    let query;
+    const reqQuery = { ...req.query };
+    const removeFields = ["select"];
+    removeFields.forEach((field) => delete reqQuery[field]);
+    let queryStr = JSON.stringify(reqQuery);
+    queryStr = queryStr.replace(
+      /\b(gt|gte|lt|lte|in)\b/,
+      (match) => `$${match}`
+    );
+    query = Bootcamp.find(JSON.parse(queryStr));
+    if (req.query.select) {
+      const fields = req.query.select.split(",").join(" ");
+      query = query.select(fields);
     }
-
-}
+    if (req.query.sort) {
+      const sortby = req.query.sort.split(",").join(" ");
+      query = query.sort(sortby);
+    } else {
+      query = query.sort("-createdAt");
+    }
+    const bootcamps = await query;
+    res
+      .status(200)
+      .json({ success: true, count: bootcamps.length, data: bootcamps });
+  } catch (error) {
+    next(new ErrorResponse(`Bootcampp not found`, 404));
+  }
+};
 
 // @desc    Get single bootcamps
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootCamp = async(req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findById(req.params.id)
-        if (!bootcamp) {
-            res.status(404).json({success:false, data:`${req.params.id} not found `});
-        }
-        res.status(200).json({success: true, data: bootcamp})
-    } catch (error) {
-        next(error)
+exports.getBootCamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      res
+        .status(404)
+        .json({ success: false, data: `${req.params.id} not found ` });
     }
-}
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    next(error);
+  }
+};
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootCamp = async(req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.create(req.body)
-        res.status(201).json({success: true, data: bootcamp})
-    } catch (error) {
-        res.status(400).json({
-            success:false,
-            error:error
-        })
-    }
-
-}
+exports.createBootCamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error,
+    });
+  }
+};
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootCamp = async(req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body,{
-        new: true,
-        runValidators: true
-    })
-    if (!bootcamp) {
-        next(error)
-    }
-    res.status(200).json({success: true, data: bootcamp})
-}
+exports.updateBootCamp = async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!bootcamp) {
+    next(error);
+  }
+  res.status(200).json({ success: true, data: bootcamp });
+};
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootCamp = async(req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
-        if (!bootcamp) {
-            next(error)
-        }
-        res.status(200).json({success: true, data: {}})
-    } catch (error) {
-        next(error)
+exports.deleteBootCamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    if (!bootcamp) {
+      next(error);
     }
-
-}
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    next(error);
+  }
+};
 
 // @desc    Get Bootcamp by Radius
 // @route   GET /api/v1/bootcamps/radius/:postcode/:distance
 // @access  Private
-exports.getBootCampByRadius = async(req, res, next) => {
-    try {
-        const {postcode, distance} = req.params
-        const loc = await geocoder.geocode(postcode)
-        const lat = loc[0].latitude
-        const lng = loc[0].longitude
+exports.getBootCampByRadius = async (req, res, next) => {
+  try {
+    const { postcode, distance } = req.params;
+    const loc = await geocoder.geocode(postcode);
+    const lat = loc[0].latitude;
+    const lng = loc[0].longitude;
 
-        const radius = distance/3963 //Radius of earth is 3963
+    const radius = distance / 3963; //Radius of earth is 3963
 
-        const bootcamps = await Bootcamp.find({
-            location: { $geoWithin:{ $centerSphere: [ [lng,lat], radius] } }
-        })
+    const bootcamps = await Bootcamp.find({
+      location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+    });
 
-        res.status(200).json({
-            success: true,
-            count: bootcamps.length,
-            data: bootcamps
-        })
-    } catch (error) {
-        next(error)
-    }
-
-}
+    res.status(200).json({
+      success: true,
+      count: bootcamps.length,
+      data: bootcamps,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
