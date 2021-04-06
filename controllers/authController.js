68982c0877acfc1168b4eaed2b73c0c5f3fc4aa9
tosin <famurewatosin@gmail.com>
2021-04-06 20:55:53 +0100

    login feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,3 +27,33 @@ exports.register = async(req, res, next)=>{
     }
 
 }
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+
+exports.login = async(req, res, next)=>{
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password){
+            next(new ErrorResponse(`Please Provide and email and password`, 400))
+        }
+
+        const user = await User.findOne({email:email}).select('+password')
+
+        if (!user) {
+            next(new ErrorResponse(`User not found`, 401))
+        }
+        const isMatch = await user.matchPassword(password)
+        if (!isMatch) {
+            next(new ErrorResponse(`User not found`, 401))
+        }
+        const token = user.getSignedJwtToken()
+    
+        res.status(200).json({success: true, token: token})
+    } catch (error) {
+        next(error)
+    }
+
+}
--- models/User.js
+++ models/User.js
@@ -46,4 +46,7 @@ UserSchema.methods.getSignedJwtToken = function () {
     });
   };
 
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 module.exports = mongoose.model("User", UserSchema);
