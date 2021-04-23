bd87547892ca9004e2a9f8a53cb5ce757e89ab0f
tosin <famurewatosin@gmail.com>
2021-04-23 03:12:20 +0100

    logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -171,3 +171,18 @@ exports.updatePassword = async (req, res, next) => {
     next(error);
   }
 };
+
+// @desc      Log user out / clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Public
+exports.logout = async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,8 @@ const {
   login,
   getMe,
   updatePassword,
-  resetPassword
+  resetPassword,
+  logout
 } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 
@@ -13,6 +14,7 @@ const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect,getMe);
 router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
