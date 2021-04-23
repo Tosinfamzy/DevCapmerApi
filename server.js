56998fb3f33cc5f672fe013ac263bea3be1e498c
tosin <famurewatosin@gmail.com>
2021-04-23 03:46:11 +0100

    add cors

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
-const mongoSanitize = require('express-mongo-sanitize');
+const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
-const xss = require('xss-clean');
+const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
-const hpp = require('hpp');
-
+const hpp = require("hpp");
+const cors = require("cors");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -23,11 +23,11 @@ const auth = require("./routes/auth");
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(hpp());
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 100 
+  windowMs: 15 * 60 * 1000,
+  max: 100,
 });
 app.use(limiter);
 
@@ -37,6 +37,7 @@ if (process.env.NODE_ENV === "development") {
 app.use(mongoSanitize());
 app.use(helmet());
 app.use(xss());
+app.use(cors())
 
 app.use("/api/v1/bootcamps", bootcamp);
 app.use("/api/v1/courses", courses);
